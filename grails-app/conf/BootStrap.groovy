297dfb65e7a96e4f49421cb3768deafb72210540
diego.mizrahi <diego.mizrahi@globallogic.com>
2013-09-07 15:09:31 -0300

    new version

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -64,13 +64,13 @@ class BootStrap {
 
 					def cinema2 = Cinema.findAllByTheater(theater)?: new Cinema(
 						cinemaNumber: 2,
-						cinemaType: CinemaType.CINEMA3D,
+						cinemaType: CinemaType.XD,
 						theater: theater
 						).save(flush: true)
 
 					def cinema3 = Cinema.findAllByTheater(theater)?: new Cinema(
 						cinemaNumber: 1,
-						cinemaType: CinemaType.CINEMA3D,
+						cinemaType: CinemaType.XD,
 						theater: theater2
 						).save(flush: true)
 
